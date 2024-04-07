abacc276d675ac65ef5eba3eca3e4bf22ef6a833
andreigeana71 <niculae.geana@student.unitbv.ro>
2024-04-07 16:42:38 +0300

    Add functionality to About menu option. Add template "About" window.

--- Checkers-Game/Checkers-Game/MainWindow.xaml.cs
+++ Checkers-Game/Checkers-Game/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Checkers_Game.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,35 +24,17 @@ namespace Checkers_Game
         public MainWindow()
         {
             InitializeComponent();
-            //var board = this.FindName("Board") as Grid;
+        }
 
-            //int n = 8;
-            //for (int i = 0; i < n; i++)
-            //{
-            //    ColumnDefinition column = new ColumnDefinition();
-            //    board.ColumnDefinitions.Add(column);
-            //}
+        private void ExitItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
 
-            //for (int i = 0; i < n; i++)
-            //{
-            //    RowDefinition row = new RowDefinition();
-            //    board.RowDefinitions.Add(row);
-            //}
-            //for(int i=0; i < n; i++)
-            //{
-            //    for(int j=0; j < n; j++)
-            //    {
-            //        Button button = new Button
-            //        {
-            //            Content = '(' + i.ToString() + ',' + j.ToString() +')'
-            //        };
-            //        button.Margin = new Thickness(2);
-            //        Grid.SetRow(button, i);
-            //        Grid.SetColumn(button, j);
-            //        board.Children.Add(button);
-            //    }
-            //}
-            
+        private void AboutItem_Click(object sender, RoutedEventArgs e)
+        {
+            Window aboutWindow = new AboutView();
+            aboutWindow.Show();
         }
     }
 }
